d93c49019648b334a9b3184454fd89250eb73c53
Alyssa <alyssadejordy@gmail.com>
2024-10-19 23:22:41 -0700

    add paragraph

---
+++ script.js
@@ -0,0 +1,9 @@
+// Create a Tic-Tac-Toe game grid using your HTML element of choice.
+// When a cell in the grid is clicked, an X or O should appear in that spot depending on whose turn it is.
+// A heading should say whether it is X's or O's turn and change with each move made.
+// A button should be available to clear the grid and restart the game.
+// When a player has won, or the board is full and the game results in a draw, a Bootstrap alert or similar Bootstrap component should appear across the screen announcing the winner.
+
+// A heading should say whether it is X's or O's turn and change with each move made.
+// --loop through between each player
+// --Find a way to display play on page//Player 1 - X; Player 2 - O
